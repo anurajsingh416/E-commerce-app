96e7f8969c150f6f0499a0da3c398d5ce14fa128
unknown <ANURAJ@LAPTOP-EE1F45K8>
2024-07-08 21:47:27 +0530

    Updated axiosInstance.js with new configurations

--- e-commerce-app/src/utils/axiosInstance.js
+++ e-commerce-app/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'e-commerce-app-backend-beta.vercel.app',
     timeout: 10000,
     headers: {
         "Content-Type":"application/json"
