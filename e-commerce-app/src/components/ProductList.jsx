1c4e863c47651897488d451cd4d139d214c3e861
unknown <ANURAJ@LAPTOP-EE1F45K8>
2024-07-09 10:50:24 +0530

    style update

--- e-commerce-app/src/components/ProductList.jsx
+++ e-commerce-app/src/components/ProductList.jsx
@@ -50,9 +50,9 @@ export default function ProductList() {
             <div className={`mx-auto w-full lg:w-full px-8 grid   ${productPage ? "grid-cols-2 lg:grid-cols-5" : "grid-cols-2 lg:grid-cols-3"} gap-6 place-items-center `}>
                 {products.map((product, index) => (
                     
-                    <div key={index} className='max-w-68 p-4 bg-gray-200 flex flex-col justify-between group transition-all'>
+                    <div key={index} className='w-52 p-2 bg-gray-200 flex flex-col justify-between group transition-all'>
                         <Link to={`/product/${product._id}`}>
-                        <img src={product.image} className='h-52 w-full object-contain hover:scale-95 transition-all duration-300'/>
+                        <img src={product.image} className='h-40 w-full object-contain hover:scale-95 transition-all duration-300'/>
                         <h2 className='text-gray-800 font-bold text-center'>{product.name}</h2>
                         <p className='text-800 mt-2 font-semibold'> Rs. {product.price}</p>
                         </Link>
